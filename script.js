bcc7462444120e15dbc9e12f1388e3b1bb1c9bac
Brandon Moye <85130587+Brandon-Moye@users.noreply.github.com>
2022-03-22 20:33:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,42 @@
 //     console.log("this is clicked");
 // })
 
-document.querySelector(".imageContainer").addEventListener("mouseover", function () {
-      document.getElementById("githubLogo").classList.add("githubLogo2");
-    });
+$(document).ready(function () {
+  if (window.location.hash) {
+    //bind to scroll function
+    $(document).scroll(function () {
+      var hash = window.location.hash;
+      var hashName = hash.substring(1, hash.length);
+      var element;
 
-    document.querySelector(".project1Description").addEventListener("mouseover", function () {
-        document.getElementById("githubLogo").classList.remove("githubLogo2");
-      });
-  
+      //if element has this id then scroll to it
+      if ($(hash).length != 0) {
+        element = $(hash);
+      }
+      //catch cases of links that use anchor name
+      else if ($('a[name="' + hashName + '"]').length != 0) {
+        //just use the first one in case there are multiples
+        element = $('a[name="' + hashName + '"]:first');
+      }
+
+      //if we have a target then go to it
+      if (element != undefined) {
+        window.scrollTo(0, element.position().top);
+      }
+      //unbind the scroll event
+      $(document).unbind("scroll");
+    });
+  }
+});
+
+document
+  .querySelector(".imageContainer")
+  .addEventListener("mouseover", function () {
+    document.getElementById("githubLogo").classList.add("githubLogo2");
+  });
+
+document
+  .querySelector(".project1Description")
+  .addEventListener("mouseover", function () {
+    document.getElementById("githubLogo").classList.remove("githubLogo2");
+  });
